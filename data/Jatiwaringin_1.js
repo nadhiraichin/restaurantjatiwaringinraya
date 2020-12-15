4ab6180f1af300226005857f9f291d711ba847e2
nadhiraichin <75218108+nadhiraichin@users.noreply.github.com>
2020-12-15 21:41:49 +0700

    Add files via upload

---
+++ data/Jatiwaringin_1.js
@@ -0,0 +1,1 @@
+var json_Jatiwaringin_1 = {"type":"FeatureCollection","name":"Jatiwaringin_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[106.911677647945695,-6.268216766557131]}}]}
